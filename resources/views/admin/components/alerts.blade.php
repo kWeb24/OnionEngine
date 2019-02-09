d87cba1c5b2648d0fde8d082fada6c758a6cbfa2
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 15:08:02 +0100

    Cleanup: Alert component

--- resources/views/admin/components/alerts.blade.php
+++ resources/views/admin/components/alerts.blade.php
@@ -1,24 +1,16 @@
+{{--
+ Available alerts:
+ 
+ alert alert-primary
+ alert alert-secondary
+ alert alert-success
+ alert alert-danger
+ alert alert-warning
+ alert alert-info
+ alert alert-light
+ alert alert-dark
+--}}
+
 <div class="alert alert-primary" role="alert">
   This is a primary alert—check it out!
 </div>
-<!-- <div class="alert alert-secondary" role="alert">
-  This is a secondary alert—check it out!
-</div>
-<div class="alert alert-success" role="alert">
-  This is a success alert—check it out!
-</div>
-<div class="alert alert-danger" role="alert">
-  This is a danger alert—check it out!
-</div>
-<div class="alert alert-warning" role="alert">
-  This is a warning alert—check it out!
-</div>
-<div class="alert alert-info" role="alert">
-  This is a info alert—check it out!
-</div>
-<div class="alert alert-light" role="alert">
-  This is a light alert—check it out!
-</div>
-<div class="alert alert-dark" role="alert">
-  This is a dark alert—check it out!
-</div> -->
