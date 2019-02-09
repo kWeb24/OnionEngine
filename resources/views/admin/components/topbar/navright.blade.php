6799915d4795c2d2ab288ba29d381018182def31
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 15:14:45 +0100

    Cleanup: Admin template top bar

---
+++ resources/views/admin/components/topbar/navright.blade.php
@@ -0,0 +1,5 @@
+<ul class="nav-right">
+  @include('OnionEngineAdmin::components.topbar.notifications')
+  @include('OnionEngineAdmin::components.topbar.messages')
+  @include('OnionEngineAdmin::components.topbar.usernav')
+</ul>
---
+++ resources/views/admin/components/topbar/usernav.blade.php
@@ -0,0 +1,37 @@
+<li class="dropdown">
+  <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
+    <div class="peer mR-10">
+      <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/10.jpg" alt="">
+    </div>
+    <div class="peer">
+      <span class="fsz-sm c-grey-900">John Doe</span>
+    </div>
+  </a>
+  <ul class="dropdown-menu fsz-sm">
+    <li>
+      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+        <i class="ti-settings mR-10"></i>
+        <span>Setting</span>
+      </a>
+    </li>
+    <li>
+      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+        <i class="ti-user mR-10"></i>
+        <span>Profile</span>
+      </a>
+    </li>
+    <li>
+      <a href="email.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+        <i class="ti-email mR-10"></i>
+        <span>Messages</span>
+      </a>
+    </li>
+    <li role="separator" class="divider"></li>
+    <li>
+      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+        <i class="ti-power-off mR-10"></i>
+        <span>Logout</span>
+      </a>
+    </li>
+  </ul>
+</li>
