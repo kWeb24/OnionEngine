d64f8901f6f24d5fbda9f45cf1473ba4feebe0a0
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 19:13:52 +0100

    Add: Dashboard title

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
   @include('OnionEngineAdmin::components.alerts')
 
+  <h4 class="c-grey-900 mT-15 mB-15">Dashboard</h4>
   <div class="row gap-20 masonry pos-r">
     <div class="masonry-sizer col-md-6"></div>
 
     @include('OnionEngineAdmin::panels.onioninfo')
-
   </div>
 @endsection
