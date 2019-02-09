3b2c9ec30a9c7d7ffa569dbf0ef3c8f0e6971f76
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 19:15:39 +0100

    Add: Use Facade

--- resources/views/admin/panels/onioninfo.blade.php
+++ resources/views/admin/panels/onioninfo.blade.php
@@ -3,7 +3,7 @@
     <h6 class="c-grey-900"><strong>OnionEngine</strong></h6>
     <div class="mT-15">
       <strong>Version:</strong> {{\OnionEngine::version()}} (Up to date)<br />
-      <strong>Stability:</strong> dev-develop<br />
+      <strong>Stability:</strong> {{\OnionEngine::stability()}}<br />
       <strong>GitHub:</strong> <a href="https://github.com/kWeb24/OnionEngine">OnionEngine</a>
     </div>
   </div>
