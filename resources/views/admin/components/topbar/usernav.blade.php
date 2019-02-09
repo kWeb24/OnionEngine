8f9c164bce7a4ca7a8bbdb8df06b938e8042eb92
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 19:17:50 +0100

    Add: Dynamic avatar

--- resources/views/admin/components/topbar/usernav.blade.php
+++ resources/views/admin/components/topbar/usernav.blade.php
@@ -1,37 +1,23 @@
 <li class="dropdown">
   <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
     <div class="peer mR-10">
-      <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/10.jpg" alt="">
+      <img class="w-2r bdrs-50p" src="{{ Avatar::create(Auth::user()->name)->toBase64() }}" alt="">
     </div>
     <div class="peer">
-      <span class="fsz-sm c-grey-900">John Doe</span>
+      <span class="fsz-sm c-grey-900">{{Auth::user()->name}}</span>
     </div>
   </a>
   <ul class="dropdown-menu fsz-sm">
-    <li>
-      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-        <i class="ti-settings mR-10"></i>
-        <span>Setting</span>
-      </a>
-    </li>
-    <li>
-      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-        <i class="ti-user mR-10"></i>
-        <span>Profile</span>
-      </a>
-    </li>
-    <li>
-      <a href="email.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
-        <i class="ti-email mR-10"></i>
-        <span>Messages</span>
-      </a>
-    </li>
     <li role="separator" class="divider"></li>
     <li>
-      <a href="" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+      <a href="{{ route('logout') }}" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700"
+         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         <i class="ti-power-off mR-10"></i>
         <span>Logout</span>
       </a>
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+      </form>
     </li>
   </ul>
 </li>
