fec2d4ec68cbcd1d21c5eee840d28fa09f5db628
Kamil Weber <kamilweber24@gmail.com>
2019-02-09 19:03:21 +0100

    Update: Namespace

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@
 */
 
 Route::group(['middleware' => ['web', 'auth', /*'role:super-admin'*/]], function() {
-  Route::get('/admin', 'kweber\OnionEngine\App\Http\Controllers\AdminController@index')->name('admin');
+  Route::get('/admin', 'Kweber\OnionEngine\App\Http\Controllers\AdminController@index')->name('admin');
 });
