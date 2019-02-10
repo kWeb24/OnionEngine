f98c40d94de42e41b12b3b466ff7a371ad7d1d34
Kamil Weber <kamilweber24@gmail.com>
2019-02-10 09:07:41 +0100

    Add: Doc block

--- src/App/Traits/Auth/OnionEngineLogin.php
+++ src/App/Traits/Auth/OnionEngineLogin.php
@@ -4,6 +4,11 @@ namespace Kweber\OnionEngine\App\Traits\Auth;
 
 trait OnionEngineLogin
 {
+  /**
+   * Show the application's login form.
+   *
+   * @return \Illuminate\Http\Response
+   */
   public function showOnionEngineLoginForm() {
     return view('OnionEngineAdmin::auth.login');
   }
