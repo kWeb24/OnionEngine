11cb3600b4dbd2008927c7e052cdeb10e07b49c6
Kamil Weber <kamilweber24@gmail.com>
2019-02-10 16:13:55 +0100

    Add: HasRoles trait to user

--- src/App/Traits/OnionEngineUser.php
+++ src/App/Traits/OnionEngineUser.php
@@ -2,8 +2,12 @@
 
 namespace Kweber\OnionEngine\App\Traits;
 
+use Spatie\Permission\Traits\HasRoles;
+
 trait OnionEngineUser
 {
+    use HasRoles;
+    
     public function test()
     {
         return 'User trait test method';
