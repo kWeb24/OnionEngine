91b911033dbdbd55e3403e384214f328aa48bd45
Kamil Weber <kamilweber24@gmail.com>
2019-02-10 09:08:29 +0100

    Add: Registration route override

--- routes/auth.php
+++ routes/auth.php
@@ -7,3 +7,4 @@
 */
 
 Route::get('/login', ['uses' => '\App\Http\Controllers\Auth\LoginController@showOnionEngineLoginForm', 'as' => 'login']);
+Route::get('/register', ['uses' => '\App\Http\Controllers\Auth\RegisterController@showOnionEngineRegistrationForm', 'as' => 'register']);
