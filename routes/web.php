4088054e574e4132f4c5b7c8e35e78497a00e3d0
Kamil Weber <kamilweber24@gmail.com>
2019-02-10 16:14:17 +0100

    Add: Use role middleware

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Route::group(['middleware' => ['web', 'auth']], function () {
     Route::get('/home', 'HomeController@dashboard')->name('home');
 });
 
-Route::group(['middleware' => ['web', 'auth' /*'role:super-admin'*/]], function () {
+Route::group(['middleware' => ['web', 'auth', 'role:super-admin']], function () {
     Route::get('/admin', '\Kweber\OnionEngine\App\Http\Controllers\AdminController@index')->name('admin');
 });
